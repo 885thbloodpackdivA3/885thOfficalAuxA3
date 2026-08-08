4724aaa9334e74c05f9883bad43202c54c9bc3b0
Chief305 <99970173+Chief305@users.noreply.github.com>
2026-08-08 10:24:22 -0400

    Update config.cpp

--- addons/885_Weapons/config.cpp
+++ addons/885_Weapons/config.cpp
@@ -7584,7 +7584,7 @@ class CfgWeapons
 				compatibleItems[]=
 				{
 					"ShadwCmpany_optic_Acog",
-					"ShadwCmpany_Electroscope",
+					"ShadwCmpany_Electroscope_Standalone",
 					"ShdwCmpny_Optic_A280Scope",
 					"ShdwCmpny_Optic_LEScope_DC15A_mk2",
 					"ShdwCmpny_Optic_LEScope_DC15A",
@@ -14270,7 +14270,6 @@ class CfgWeapons
 					"ShadwCmpany_Hybrid_Magnifier_Magnified",
 					"ShadwCmpany_773_Hybrid_Magnifier_Holo",
 					"ShadwCmpany_773_Hybrid_Magnifier_Magnified",
-					"ShadwCmpany_Electroscope",
 					"ShadwCmpany_Electroscope_Standalone",
 					"ShadwCmpany_optic_e11",
 					"ShdwCmpny_Optic_Scope_WestarM5",
@@ -15006,7 +15005,6 @@ class CfgWeapons
 					"ShadwCmpany_Hybrid_Magnifier_Magnified",
 					"ShadwCmpany_773_Hybrid_Magnifier_Holo",
 					"ShadwCmpany_773_Hybrid_Magnifier_Magnified",
-					"ShadwCmpany_Electroscope",
 					"ShadwCmpany_Electroscope_Standalone",
 					"ShadwCmpany_optic_e11",
 					"ShdwCmpny_Optic_Scope_WestarM5",
@@ -16775,7 +16773,6 @@ class CfgWeapons
 					"ShadwCmpany_Hybrid_Magnifier_Magnified",
 					"ShadwCmpany_773_Hybrid_Magnifier_Holo",
 					"ShadwCmpany_773_Hybrid_Magnifier_Magnified",
-					"ShadwCmpany_Electroscope",
 					"ShadwCmpany_Electroscope_Standalone",
 					"ShadwCmpany_optic_e11",
 					"ShdwCmpny_Optic_Valken",
