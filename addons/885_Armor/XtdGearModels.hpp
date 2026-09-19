3d8bdd5cb9ad4567c151d422000e155df75fb849
ONIBushido <885thbloodpackdiv@gmail.com>
2026-09-19 10:32:09 -0500

    Oxlong Backpack
    Oxlong Backpack - Stable
    Added to Ext. Arsenal

--- addons/885_Armor/XtdGearModels.hpp
+++ addons/885_Armor/XtdGearModels.hpp
@@ -633,6 +633,7 @@ class XtdGearModels
 					"Bandit",
 					"Charge",
 					"Nightfall",
+					"Oxlong",
 					"Rios",
 					"Scorch",
 					"Thunder"
@@ -1689,6 +1690,11 @@ class XtdGearInfos
 		{
 			model="Custom_Backpacks";
 			backpack="Nightfall";
+		};
+		class 885th_Oxlong_Backpack
+		{
+			model = "Custom Backpacks";
+			backpack = "Oxlong";
 		};
 		class 885th_Cust_RTO_Buttbag2_Rios
 		{
